ae777bd38a342092382dd682f7eb6d957824c332
Aaron Kimball <akimball83@gmail.com>
2021-12-31 13:52:40 -0800

    Make `empty` app not *completely* empty.
    * ... Now it blinks.
    * Pull in a super-modest set of core.a for baseline.

--- empty/empty.cpp
+++ empty/empty.cpp
@@ -1,14 +1,28 @@
+// (c) Copyright 2021 Aaron Kimball
+//
+// A very simple pin-blinking program with a "minimal program size"
+// to use as a benchmark for how much overhead libraries add.
+//
+// This makes use of *some* functionality from Arduino.h that is very common
+// so we don't unfairly charge the library for making modest use of the Arduino core;
+// it would otherwise virtually all be stripped out.
 #include<Arduino.h>
 
 #define DEBUG
-// #define DBG_WAIT_FOR_CONNECT
-// #define DBG_START_PAUSED
 #define DBG_PRETTY_FUNCTIONS
-//#include <dbg.h>
+#include <dbg.h>
 
+static const int PIN = 5;
 
-void setup() {
+SETUP() {
+  pinMode(PIN, OUTPUT);
 }
+
+static uint8_t state = 0;
 
 void loop() {
+  state = !state;
+  digitalWrite(PIN, state);
+  delay(500);
 }
+
