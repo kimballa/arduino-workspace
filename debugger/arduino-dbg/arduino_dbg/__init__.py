26c0bf25c45be01edf707687c794dd66a6f3e1d9
Aaron Kimball <akimball83@gmail.com>
2022-01-29 12:43:47 -0800

    Add version string.

--- debugger/arduino-dbg/arduino_dbg/__init__.py
+++ debugger/arduino-dbg/arduino_dbg/__init__.py
@@ -9,12 +9,17 @@ from .term import ConsolePrinter
 import arduino_dbg.dump as dump
 import arduino_dbg.io as io
 
+DBG_VERSION = [ 0, 1, 0 ]
+DBG_VERSION_STR = '.'.join(map(str, DBG_VERSION))
+FULL_DBG_VERSION_STR = f'Arduino Debugger (adbg) version {DBG_VERSION_STR}'
+__version__ = DBG_VERSION_STR
 
 def _parseArgs():
     parser = argparse.ArgumentParser(description="Serial debugger client for Arduino apps")
     parser.add_argument("-p", "--port")
     parser.add_argument("-f", "--file", metavar="elf_file")
     parser.add_argument("-d", "--dump", metavar="dump_file")
+    parser.add_argument("-v", "--version", action="version", version=FULL_DBG_VERSION_STR)
 
     return parser.parse_args()
 
